0dc4a7f0badd45e46fc158a0bd37114aeee6d0f9
Justin Somers <justin.somers007@gmail.com>
2018-01-16 10:53:53 -0600

    added working nav-bar

--- my-portfolio/src/app/app.module.ts
+++ my-portfolio/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { IsBootstrapWorkingComponent } from './is-bootstrap-working/is-bootstrap-working.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    IsBootstrapWorkingComponent
+    IsBootstrapWorkingComponent,
+    NavBarComponent
   ],
   imports: [
     FormsModule,
