2c996a333697b41513af8c45e69da349518be063
Justin Somers <justin.somers007@gmail.com>
2018-01-24 15:47:51 -0600

    added about component

--- my-portfolio/src/app/components/about/about.component.ts
+++ my-portfolio/src/app/components/about/about.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  profileImg: string;
+
+  constructor() {
+    this.profileImg = '../../../assets/imgs/trumpet.jpg';
+   }
 
   ngOnInit() {
   }
