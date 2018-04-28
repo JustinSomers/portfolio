a2cb00621d4aa68ce0dc6cd6e324e064e227251a
justinsomers <justin.somers007@gmail.com>
2018-04-27 23:12:29 -0500

    deleted log

---
+++ PortfolioWebApi/PortfolioWebApi/Data/ArticleContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWebApi.Models
+{
+    public class ArticleContext: DbContext
+    {
+        public ArticleContext(DbContextOptions<ArticleContext> options) : base(options)
+        {}
+
+        public DbSet<Article> Articles { get; set; }
+    }
+}
